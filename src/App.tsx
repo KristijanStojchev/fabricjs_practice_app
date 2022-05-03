4fab178a5366910589e7ddaa1ee0941f32226138
Kristijan <kristijan.stojcev1@gmail.com>
2022-05-03 15:31:21 +0200

    Initial Version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fabric } from "fabric";
+const App = () => {
+  const canvas = new fabric.Canvas("canvas", {
+    width: 800,
+    height: 800,
+    backgroundColor: "red",
+  });
 
-function App() {
+  let [valHeight, setvalHeight] = useState("");
+  let [valWidth, setvalWidth] = useState("");
+  let [valX, setvalX] = useState("");
+  let [valY, setvalY] = useState("");
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  const generateRect = () => {
+    const rect = new fabric.Rect({
+      originX: "top",
+      originY: "left",
+      top: parseInt(valX),
+      left: parseInt(valY),
+      height: parseInt(valHeight),
+      width: parseInt(valWidth),
+      fill: "blue",
+    });
+    canvas.add(rect);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={(event) => setvalHeight(event.target.value)} />
+        <input onChange={(event) => setvalWidth(event.target.value)} />
+        <input onChange={(event) => setvalX(event.target.value)} />
+        <input onChange={(event) => setvalY(event.target.value)} />
+        <button type="submit" onClick={() => generateRect()}>
+          Submit
+        </button>
+      </form>
+
+      <canvas id="canvas" />
     </div>
   );
-}
+};
 
 export default App;
