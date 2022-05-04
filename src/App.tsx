8e5058d7abc8ceabd8f23915d836e1b263770ce5
Kristijan <kristijan.stojcev1@gmail.com>
2022-05-04 14:46:46 +0200

    Added labels, min/max values, saving objects on canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import { fabric } from "fabric";
 const App = () => {
-  const canvas = new fabric.Canvas("canvas", {
-    width: 800,
-    height: 800,
-    backgroundColor: "red",
-  });
+  const [canvas, setCanvas] = useState("");
+
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, []);
+
+  const initCanvas = () =>
+    new fabric.Canvas("canvas", {
+      height: 800,
+      width: 800,
+      backgroundColor: "red",
+    });
+
+  // const canvas = new fabric.Canvas("canvas", {
+  //   width: 800,
+  //   height: 800,
+  //   backgroundColor: "red",
+  // });
 
   let [valHeight, setvalHeight] = useState("");
   let [valWidth, setvalWidth] = useState("");
   let [valX, setvalX] = useState("");
   let [valY, setvalY] = useState("");
 
-  const onSubmit = (e: any) => {
+  const generateRect = (e: any, canvi: any) => {
     e.preventDefault();
-  };
-
-  const generateRect = () => {
-    const rect = new fabric.Rect({
-      originX: "top",
-      originY: "left",
-      top: parseInt(valX),
-      left: parseInt(valY),
-      height: parseInt(valHeight),
-      width: parseInt(valWidth),
-      fill: "blue",
-    });
-    canvas.add(rect);
+    if (
+      parseInt(valHeight) <= 0 ||
+      parseInt(valHeight) > 800 ||
+      parseInt(valWidth) <= 0 ||
+      parseInt(valWidth) > 800
+    ) {
+      alert("height and width must be bigger than 0 and smaller than 800");
+    } else {
+      const rect = new fabric.Rect({
+        originX: "left",
+        originY: "top",
+        top: parseInt(valX),
+        left: parseInt(valY),
+        height: parseInt(valHeight),
+        width: parseInt(valWidth),
+        fill: "blue",
+      });
+      canvi.add(rect);
+      canvi.renderAll();
+    }
   };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={(event) => setvalHeight(event.target.value)} />
-        <input onChange={(event) => setvalWidth(event.target.value)} />
-        <input onChange={(event) => setvalX(event.target.value)} />
-        <input onChange={(event) => setvalY(event.target.value)} />
-        <button type="submit" onClick={() => generateRect()}>
-          Submit
-        </button>
+      <form onSubmit={(e) => generateRect(e, canvas)}>
+        <label htmlFor="height">Height: </label>
+        <input
+          id="height"
+          onChange={(event) => setvalHeight(event.target.value)}
+        />
+        <label htmlFor="width"> Width: </label>
+        <input
+          id="width"
+          onChange={(event) => setvalWidth(event.target.value)}
+        />
+        <label htmlFor="y"> Y coordinate: </label>
+        <input id="y" onChange={(event) => setvalX(event.target.value)} />
+        <label htmlFor="x"> X coordinate: </label>
+        <input id="x" onChange={(event) => setvalY(event.target.value)} />
+        <button type="submit">Generate Rectangle</button>
       </form>
-
       <canvas id="canvas" />
     </div>
   );
